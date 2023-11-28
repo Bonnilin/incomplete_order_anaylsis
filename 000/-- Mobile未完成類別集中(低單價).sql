824e90bb00fe0aa57a433ab175542918f7d08414
Bonnilin <152203322+Bonnilin@users.noreply.github.com>
2023-11-28 13:48:53 +0800

    Add files via upload

--- 000/-- Mobile未完成類別集中(低單價).sql
+++ 000/-- Mobile未完成類別集中(低單價).sql
@@ -1,9 +1,8 @@
-SELECT
-b.item_price_usd,
-count(DISTINCT a.order_id) AS ' incomplete_order'
-FROM order_tab a
-LEFT JOIN order_item_tab b ON a.order_id = b.order_id
-LEFT JOIN item_info_tab c ON b.item_id = c.item_id
-LEFT JOIN item_categories_tab d ON c.category_id = d.category_id
-WHERE b.status IN (1,2,6) AND c.category_id = 16
-GROUP BY 1
+SELECT b.item_price_usd,
+       count(DISTINCT a.order_id) AS ' incomplete_order'
+     FROM order_tab a
+     LEFT JOIN order_item_tab b ON a.order_id = b.order_id
+     LEFT JOIN item_info_tab c ON b.item_id = c.item_id
+     LEFT JOIN item_categories_tab d ON c.category_id = d.category_id
+     WHERE b.status IN (1,2,6) AND c.category_id = 16
+     GROUP BY 1
